59aa9bf7b9ded136d3eb7b419eb7e10385ccca32
Shawn <xiaxiangningh@163.com>
2023-05-05 09:37:51 +0800

    change

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -10,14 +10,26 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, Worl"
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", spanish)
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("", french)
+		want := "Bonjour, World"
 
 		assertCorrectMessage(t, got, want)
 	})
